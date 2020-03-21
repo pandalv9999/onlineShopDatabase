3c1edbc1ca97f26444a3db265e914bd65bc93d52
Rujun Yao <rujunyao@Rujuns-MacBook-Pro-2.local>
2020-03-20 17:37:36 -0700

    populateData, createTable bug free

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         MySQLConnection sql_conn = new MySQLConnection();
         sql_conn.createTables();
-        //sql_conn.populateData();
+        sql_conn.populateData();
         //sql_conn.createViews();
         //sql_conn.displayViews();
     }
